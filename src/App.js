9a10ef1bbd36749d460ae33fc63a96c4fa20a5e2
Lap Van Pham <caramel-apple@outlook.com>
2022-04-27 14:09:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keypressed: '',
+    };
+  }
+
+  handleKeyPress = (e) => {
+    console.log(e.key);
+    this.setState(() => ({
+      keypressed: e.key,
+    }));
+
+    console.log('current state', this.state);
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  render() {
+    return (
+      <div className='App' onKeyDown={this.handleKeyPress}>
+        <h1>
+          Key Pressed: <span>{this.state.keypressed}</span>
+        </h1>
+      </div>
+    );
+  }
 }
-
 export default App;
